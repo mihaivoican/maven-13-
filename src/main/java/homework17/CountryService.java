7c3f3e82d728b2cb4c4d73bfb7b9f8a6946fef8f
Mihai Voican <mihai.voican@rarom.ro>
2022-11-22 13:40:19 +0200

    homework17

--- src/main/java/homework17/CountryService.java
+++ src/main/java/homework17/CountryService.java
@@ -4,20 +4,75 @@ import java.io.IOException;
 import java.util.List;
 
 public class CountryService {
-    List<Country> listaTari = CountryReader.listCountries("files/countries2.txt");
+    private final List<Country> listaTari;
 
     //constructor
     public CountryService() throws IOException {
+        listaTari = CountryReader.listCountries("files/countries2.txt");
     }
 
     public List<Country> getListaTari() {
         return listaTari;
     }
 
-    public List<String> countriesNames(){
+
+    //- list all country names :  -> returns a list of strings
+    public List<String> countriesNames() {
         return listaTari.stream()
-                .map(c->c.name())
+                .map(c -> c.name())
                 .toList();
     }
+
+    //- get capital of a country :  -> returns a string
+    public String getCapital(String country) {
+        return listaTari.stream()
+                .filter(c -> c.name().equals(country))
+                .map(c -> c.capital())
+                .findFirst()
+                .orElse("nu exista aceasta tara");
+
+    }
+
+    // get population of a country : -> returns a long
+    public double getPopulation(String country) {
+        return listaTari.stream()
+                .filter(c ->c.name().equals(country))
+                .map(Country::population)
+                .findFirst()
+                .orElse(0d);
+
+    }
+
+    //- get countries in continent : -> returns list of Country objects
+    public List<String> getCountryInContinent(String continent){
+        return listaTari.stream()
+                .filter(c->c.continent().equals(continent))
+                .map(Country::name)
+                .toList();
+    }
+
+    //- get country neighbours :  -> returns list of Strings
+    public List<List<String>> getCountryNeighbours (String country){
+        return listaTari.stream()
+                .filter(c->c.name().equals(country))
+                .map(Country::neighbour)
+                .toList();
+    }
+
+    //- get countries in <continent> with population larger than <population> : -> returns list of Country objects
+    public List<Country> getCountryWithPopulationGreaterThan(String continent, double population){
+        return listaTari.stream()
+                .filter(c->c.continent().equals(continent))
+                .filter(c->c.population() > population)
+                .toList();
+    }
+
+    //- get countries that neighbor X but not neighbor Y :-> returns list of Country objects
+//    public List<Country> getCountryWithNeighbourWithoutNeighbour(String tara1, String tara2){
+//        return listaTari.stream()
+//                .filter(c->c.neighbour().stream().filter(t->t.equals(tara1)))
+//                .toList();
+//    }
+
 
 }
--- src/main/java/homework17/Main.java
+++ src/main/java/homework17/Main.java
@@ -6,15 +6,42 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<Country> listaTari =  CountryReader.listCountries("files/countries2.txt");
-        System.out.println(listaTari.toString());
+
 
         //listare obiecte tari
-        CountryService serviceTari = new CountryService();
+        CountryService serviceTari = new CountryService();      //instantiere serviciu-> aici incarc lista tari
         System.out.println(serviceTari.getListaTari());
 
         //lista nume tari
         System.out.println(serviceTari.countriesNames());
+
+        //gasesc capitala tarii
+        System.out.println(serviceTari.getCapital("Afghanistan"));
+        System.out.println(serviceTari.getCapital("Austria"));
+        System.out.println(serviceTari.getCapital("Astr"));
+
+        //populatia tarii
+        System.out.println(serviceTari.getPopulation("Romania"));
+        System.out.println(serviceTari.getPopulation(null));
+
+        //tari pe continent
+        System.out.println(serviceTari.getCountryInContinent("Asia"));
+        System.out.println(serviceTari.getCountryInContinent("Europe"));
+        System.out.println(serviceTari.getCountryInContinent("ddd"));
+
+        //lista vecini /tara
+        System.out.println(serviceTari.getCountryNeighbours("Romania"));
+        System.out.println(serviceTari.getCountryNeighbours("lll"));
+
+        //lista tari in continent cu populatia mai mare decat...
+        System.out.println(serviceTari.getCountryWithPopulationGreaterThan("Europe",0));
+        System.out.println(serviceTari.getCountryWithPopulationGreaterThan("Europe",20000000));
+
+        //tari vecine cu tara1, dar nu si cu tara2
+        System.out.println(serviceTari.getCountryWithNeighbourWithoutNeighbour("ROM",null));
+
+
+
     }
 
 
